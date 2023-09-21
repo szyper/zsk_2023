cae444cd4c38f3d1cafa8c7d18f35731e9316648
szyper <zskgit@gmail.com>
2023-09-21 08:40:56 +0200

    type

---
+++ 2c_gr2/project_2_3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_2_3
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Programowanie");
+      Console.WriteLine(" w języku C#");
+
+      /*
+       * komentarz w
+       * wielu liniach
+       */
+
+      //typy danych
+      /*
+       *  byte 0-255 (8 bitów => 1 bajt)
+       *  sbyte -128-127 (8 bitów => 1 bajt)
+       *  
+       *  short -32768 - 32767 (16 bitów => 2 bajty)
+       *  ushort 0-65535 (16 bitów => 2 bajty)
+       *  
+       *  int -2 147 483 648 - 2 147 483 647 (32 bity => 4 bajty, Int32)
+       *  uint 4 294 967 295 (32 bity => 4 bajty)
+       *  
+       *  Int64 => System.Int64 => long
+       */
+
+      byte b = 0;
+      Console.WriteLine("Zmienna b wynosi: " + b);
+
+      sbyte sb = 127;
+      Console.WriteLine($"Zmienna sb wynosi: {sb}");
+
+      short s = -20000;
+      ushort us = 10000;
+      Console.WriteLine("Zmienna s wynosi: {0}, zmienna us wynosi: {1}", s, us);
+
+      Int16 i = 32767; //short
+      Int16 i1 = -32767; //short
+
+      System.Int64 i2 = 2222222222222222;
+
+      Console.ReadKey();
+    }
+  }
+}
---
+++ 2d_gr_2/project_3_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_3_3
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Programowanie");
+      Console.WriteLine(" w języku C#");
+
+      //typy danych
+      /*
+       * byte 0-255 (8 bitów => 1 bajt)
+       * sbyte -128-127 (8 bitów => 1 bajt)
+       * short -32768 - 32767 (16 bitów => 2 bajty)
+       * ushort 0 - 65535 (16 bitó => 2 bajty)
+      */
+
+      byte b = 1;
+      sbyte sb = -10;
+
+      Console.WriteLine("Zmienna b wynosi " + b);
+      Console.WriteLine($"Zmienna b wynosi {b}");
+      Console.WriteLine("Zmienna sb wynosi {1}, zmienna b wynosi {0}", b, sb);
+
+      Console.ReadKey();
+    }
+  }
+}
