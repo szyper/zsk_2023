bda385d23984fe2637e4f5cf31941d589bb65337
szyper <zskgit@gmail.com>
2023-09-25 07:53:16 +0200

    typy danych

--- 2c_gr_1/project_3_4/Program.cs
+++ 2c_gr_1/project_3_4/Program.cs
@@ -22,6 +22,18 @@ namespace project_3_4
        * sbyte -128-127 (8 bitów => 1 bajt)
        * short -32768-32767 (16 bitow => 2 bajty)
        * ushort 0-65535 (16 bitow => 2 bajty)
+       * int -2 147 483 648 - 2 147 483 647 (32 bity => 4 bajty)
+       * uint 0 - 4 294 967 295
+       * long -9 223 372 036 854 775 808 - 9 223 372 036 854 775 807 (64 bity => 8 bajtów) 
+       * ulong 0 - 18 446 744 073 709 551 615 (64 bity => 8 bajtów)
+       * 
+       * bool (true lub false)
+       * char U+0000 do U+FFFF (16 bitowy znak z tablicy Unicode)
+       * string ?
+       * 
+       * float -3.4 x 10^38  do  3.4 x 10^38 (32 bity)
+       * double (64 bity)
+       * decimal (128 bity)
        * 
        */
       int i = 10;
@@ -35,6 +47,30 @@ namespace project_3_4
       short s = -1;
       Console.WriteLine("Zmienna us wynosi: {0},zmienna s wynosi: {1}", us, s);
       Console.WriteLine("Zmienna s wynosi: {1},zmienna us wynosi: {0}", us, s);
+
+      int i1 = -10;
+      Int32 i2 = 10; //Int16, Int64
+      System.Int32 i3 = 10;
+
+      long l = 10L;
+
+      bool b1 = true;
+      Console.WriteLine(b1); //True
+
+      bool b2 = false;
+      Console.WriteLine(b2); //False
+
+      float f1 = 10.5f;
+      Console.WriteLine(f1); //10,5
+
+      float f2 = 5f;
+      Console.WriteLine(f2); //5
+
+      //sprawdzenie rozmiaru w pamięci
+      Console.WriteLine("Typ int zajmuje w pamięci: " + sizeof(int)); //4 czyli 4 bajty
+      Console.WriteLine("Typ float zajmuje w pamięci: " + sizeof(float)); //4 bajty
+      //Console.WriteLine("Typ string zajmuje w pamięci: " + sizeof(string)); //4 bajty
+      Console.WriteLine("Typ char zajmuje w pamięci: " + sizeof(char)); //2 bajty
 
       Console.ReadKey();
     }
