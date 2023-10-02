36ecee332972cdbe6c320e622d59b7856d07b28c
szyper <zskgit@gmail.com>
2023-10-02 10:11:23 +0200

    c#

--- 2c_gr_1/project_4_1/Program.cs
+++ 2c_gr_1/project_4_1/Program.cs
@@ -21,6 +21,51 @@
       string newMessage1 = message.Substring(0,1).ToUpper() + message.Substring(1).ToLower();
       Console.WriteLine(newMessage1); //Imię i nazwisko
 
+      //zakres zmiennej
+      Console.WriteLine(int.MinValue); //-2 147 483 648
+      Console.WriteLine(int.MaxValue); //2 147 483 647
+
+      Console.WriteLine(char.MaxValue); //wyświetla ?
+
+      string firstName = "Janusz";
+      Console.WriteLine(firstName.Length); //6
+
+      string lastName = "Bąk";
+      Console.WriteLine(lastName.Length); //3
+
+      Console.WriteLine(lastName[0]); //B
+
+      Console.WriteLine(firstName.Equals(lastName)); //False
+      Console.WriteLine("Janusz".Equals("Janusz")); //True
+      Console.WriteLine("janusz".Equals("Janusz")); //False
+
+      //###########################################################
+
+      Console.Write("Podaj imię:");
+      string? name = Console.ReadLine();
+
+      while (name.Length == 0)
+      {
+        Console.Write("Podaj imię:");
+        name = Console.ReadLine();
+      }
+
+      Console.WriteLine("Imię: " + name);
+
+      //###########################################################
+
+      Console.Write("Podaj długość boku kwadratu:");
+      float a = float.Parse(Console.ReadLine());
+      while (a <= 0)
+      {
+        Console.Write("Podaj długość boku kwadratu:");
+        a = float.Parse(Console.ReadLine());
+
+      }
+
+      Console.WriteLine($"Bok kwadratu wynosi: {a}, pole: {a*a}cm^2");
+
+
     }
   }
 }
--- 2d_gr1/project_4_1/Program.cs
+++ 2d_gr1/project_4_1/Program.cs
@@ -9,8 +9,54 @@
       string messageUpper = message.ToUpper();
 
       string newMessage = message[0].ToString().ToUpper() + message.Substring(1).ToLower();
-
       Console.WriteLine(newMessage);
+
+      //zakres zmiennej
+      Console.WriteLine(int.MinValue); //-2 147 483 648
+      Console.WriteLine(int.MaxValue); //2 147 483 647
+      Console.WriteLine(char.MaxValue); //?
+
+      string firstName = "Janusz";
+      Console.WriteLine(firstName.Length); //6
+      Console.WriteLine("Bąk".Length); //3
+
+      Console.WriteLine(firstName[0]); //J
+
+      string lastName = "Nowak";
+      Console.WriteLine(firstName.Equals(lastName)); //False
+      Console.WriteLine("Janusz".Equals("Janusz")); //True
+      Console.WriteLine("Janusz".Equals("janusz")); //False
+
+      //#########################################################
+
+      Console.Write("Podaj imię:");
+      string? name = Console.ReadLine();
+
+      //while (name == "")
+      while (name.Length == 0)
+      {
+        Console.Write("Podaj imię:");
+        name = Console.ReadLine();
+      }
+
+      Console.WriteLine("Imię: " + name);
+
+      //#########################################################
+
+      float? a;
+      Console.Write("Podaj bok a:");
+      a = float.Parse(Console.ReadLine());
+
+      while (a <= 0)
+      {
+        Console.Write("Podaj bok a:");
+        a = float.Parse(Console.ReadLine());
+      }
+
+      Console.WriteLine($"Bok a wynosi: {a}");
+
+      //dokończyć zadanie
+
     }
   }
 }
