012ec0862b0df9a7af5bbfe098e633a617aa1445
szyper <31780468+szyper@users.noreply.github.com>
2023-10-18 13:48:34 +0200

    zsk

---
+++ 3c_gr_1/project_2/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\ShowController;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
+
+Route::get('show', [\App\Http\Controllers\ShowController::class, 'show']);
+Route::get('show_data', [ShowController::class, 'showData']);
