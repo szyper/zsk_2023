9991dd3f7e97267f312b6fe1da1603fbb9d13790
szyper <31780468+szyper@users.noreply.github.com>
2023-10-30 07:22:02 +0100

    zsk

---
+++ 2c_gr2/project_6/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace project_6
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      //dodać pętle do while
+      Console.WriteLine("Podaj dwie liczby\n");
+      try
+      {
+        Console.Write("Podaj x:");
+        int x = int.Parse(Console.ReadLine());
+        Console.Write("Podaj y:");
+        int y = int.Parse(Console.ReadLine());
+        if ( y != 0)
+        {
+          Console.Write($"Iloraz ");
+          Console.ForegroundColor = ConsoleColor.Green;
+        }
+        
+        Console.WriteLine($"{x} / {y} = {x / y}");
+        Console.ResetColor();
+      }
+      catch(FormatException) 
+      {
+        ErrorColorChange("Podaj liczbę całkowitą");
+      }
+      catch (OverflowException)
+      {
+        ErrorColorChange($"Liczba poza zakresem <{int.MinValue} ; {int.MaxValue}>");
+      }
+      catch (DivideByZeroException)
+      {
+        ErrorColorChange("Dzielenie przez 0!");
+      }
+      catch (Exception ex)
+      {
+        //Console.WriteLine(ex.ToString());
+        Console.WriteLine(ex.Message);
+      }
+      finally
+      {
+        Console.WriteLine("Dziękujemy za skorzystanie z programu");
+      }
+
+    }
+
+    static void ErrorColorChange(string message)
+    {
+      Console.ForegroundColor = ConsoleColor.Red;
+      Console.WriteLine($"\nBłąd: {message}\n");
+      Console.ResetColor();
+    }
+  }
+}
---
+++ 2c_gr_1/project_6_try_catch/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace project_6_try_catch
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Podaj dwie liczby\n");
+      try
+      {
+        Console.Write("Podaj x:");
+        int x = int.Parse(Console.ReadLine());
+        Console.Write("Podaj y:");
+        int y = int.Parse(Console.ReadLine());
+        Console.WriteLine($"\nIloraz {x} / {y} = {x / y}");
+      }
+      catch (OverflowException)
+      {
+        Console.WriteLine($"\nPrzekroczono zakres liczby <{int.MinValue} ; {int.MaxValue}>");
+      }
+      catch (FormatException)
+      {
+        Console.WriteLine("\nBłędny format danych");
+      }
+      catch (DivideByZeroException)
+      {
+        Console.WriteLine("\nDzielenie przez 0");
+      }
+      catch (Exception ex)
+      {
+        //Console.WriteLine(ex.ToString());
+        Console.WriteLine("Błąd: " + ex.Message);
+      }
+      finally 
+      {
+        Console.WriteLine("\nDziekujemy za skorzystanie z programu");
+      }
+      
+      
+      
+    }
+  }
+}
