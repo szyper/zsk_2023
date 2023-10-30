0f4e92cbd81077acfcbb2c66bf683423cc3977a6
szyper <31780468+szyper@users.noreply.github.com>
2023-10-30 09:01:24 +0100

    zsk

--- 2c_gr_1/project_6_try_catch/Program.cs
+++ 2c_gr_1/project_6_try_catch/Program.cs
@@ -4,39 +4,56 @@
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Podaj dwie liczby\n");
-      try
-      {
-        Console.Write("Podaj x:");
-        int x = int.Parse(Console.ReadLine());
-        Console.Write("Podaj y:");
-        int y = int.Parse(Console.ReadLine());
-        Console.WriteLine($"\nIloraz {x} / {y} = {x / y}");
-      }
-      catch (OverflowException)
-      {
-        Console.WriteLine($"\nPrzekroczono zakres liczby <{int.MinValue} ; {int.MaxValue}>");
-      }
-      catch (FormatException)
-      {
-        Console.WriteLine("\nBłędny format danych");
-      }
-      catch (DivideByZeroException)
-      {
-        Console.WriteLine("\nDzielenie przez 0");
-      }
-      catch (Exception ex)
-      {
-        //Console.WriteLine(ex.ToString());
-        Console.WriteLine("Błąd: " + ex.Message);
-      }
-      finally 
-      {
-        Console.WriteLine("\nDziekujemy za skorzystanie z programu");
-      }
+      bool isCorrect = false; 
       
+      do
+      {
+        Console.WriteLine("Podaj dwie liczby\n");
+        try
+        {
+          Console.Write("Podaj x:");
+          int x = int.Parse(Console.ReadLine());
+          Console.Write("Podaj y:");
+          int y = int.Parse(Console.ReadLine());
+          if (y != 0)
+          {
+            Console.Write("\nIloraz ");
+            Console.ForegroundColor = ConsoleColor.Green;
+          }
+          Console.WriteLine($"{x} / {y} = {x / y}");
+          Console.ResetColor();
+          isCorrect = true;
+        }
+        catch (OverflowException)
+        {
+          ErrorColorChange($"Przekroczono zakres liczby <{int.MinValue} ; {int.MaxValue}>");
+        }
+        catch (FormatException)
+        {
+          ErrorColorChange("Błędny format danych");
+        }
+        catch (DivideByZeroException)
+        {
+          ErrorColorChange("Dzielenie przez 0");
+        }
+        catch (Exception ex)
+        {
+          //Console.WriteLine(ex.ToString());
+          Console.WriteLine("Błąd: " + ex.Message);
+        }
+        finally
+        {
+          Console.WriteLine("\nDziekujemy za skorzystanie z programu");
+        }
+      } while (!isCorrect);
       
-      
+    }
+
+    static void ErrorColorChange(string comment)
+    {
+      Console.ForegroundColor = ConsoleColor.Red;
+      Console.WriteLine($"\nBłąd: {comment}");
+      Console.ResetColor();
     }
   }
 }
---
+++ 2d_gr1/project_6_try_catch/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace project_6_try_catch
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Podaj dwie liczby");
+      bool isCorrect = false;
+      do
+      {
+        try
+        {
+          Console.Write("Podaj x:");
+          int x = int.Parse(Console.ReadLine());
+          Console.Write("Podaj y:");
+          int y = int.Parse(Console.ReadLine());
+          Console.WriteLine($"Iloraz {x} / {y} = {x / y}");
+          isCorrect = true;
+        }
+        catch (DivideByZeroException)
+        {
+          ErrorColorChange("Dzielenie przez 0");
+        }
+        catch (FormatException)
+        {
+          ErrorColorChange($"Błąd: błędny format danych");
+        }
+        catch (OverflowException)
+        {
+          ErrorColorChange($"Błąd: przekroczono zakres danych <{int.MinValue} ; {int.MaxValue}>");
+        }
+        catch (Exception ex)
+        {
+          ErrorColorChange($"{ex.Message}");
+        }
+        finally
+        {
+          Console.WriteLine("\nDziękujemy za skorzystanie z programu\n");
+        }
+      } while (!isCorrect);
+    }
+
+    static void ErrorColorChange(string message)
+    {
+      Console.ForegroundColor = ConsoleColor.Red;
+      Console.WriteLine($"\nBłąd: {message}");
+      Console.ResetColor();
+    }
+
+  }
+}
