df13c9ad6d91aeae4c61ca1f1c51a5e277635f1d
szyper <31780468+szyper@users.noreply.github.com>
2023-12-05 15:54:56 +0100

    zsk

---
+++ 2c_gr_1/_7_tablice_3_zadanie/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _7_tablice_3_zadanie
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      ReadNumber();
+    }
+
+    static int ReadNumber()
+    {
+      int number;
+
+      while (true)
+      {
+        Console.Write("Podaj lczbę całkowitą dodatnią:");
+        string? input = Console.ReadLine();
+
+        if (input == "q")
+        {
+          Console.WriteLine("Zakończono program");
+          Environment.Exit(0);
+        }
+
+        bool success = int.TryParse(input, out number);
+
+        if (success && number > 0) 
+        {
+          return number;
+        }
+        else
+        {
+          Console.WriteLine("Podana wartość nie jest poprawną liczbą dodatnią. Spróbuj ponownie lub wpisz qm aby zakończyć program");
+        }
+      }
+    }
+  }
+}
