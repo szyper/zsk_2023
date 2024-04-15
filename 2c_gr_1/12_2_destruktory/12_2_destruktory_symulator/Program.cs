9e0046b0531d75ec67421a8a818547782836048e
szyper <31780468+szyper@users.noreply.github.com>
2024-04-15 07:24:40 +0200

    zsk

---
+++ 2c_gr_1/12_2_destruktory/12_2_destruktory_symulator/Program.cs
@@ -0,0 +1,67 @@
+﻿using _12_2_destruktory_symulator.classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_2_destruktory_symulator
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      List<Car> cars = new List<Car>();
+      Dictionary<int, Car> carDictionary = new Dictionary<int, Car>();
+
+      while (true)
+      {
+        Console.WriteLine("Menu symulatora jazdy samochodem");
+        Console.WriteLine("1. Dodaj samochód");
+        Console.WriteLine("2. Wyświetl listę samochodów");
+        Console.WriteLine("3. Jedź samochodem");
+        Console.WriteLine("4. Symuluj losowe uszkodzenie");
+        Console.WriteLine("5. Zezłomuj samochód");
+        Console.WriteLine("6. Wyjście");
+
+        Console.Write("Wybierz opcję (1-6):");
+        int choice = int.Parse(Console.ReadLine());
+
+        switch (choice)
+        {
+          case 1:
+            Console.Write("Podaj markę:");
+            string brand = Console.ReadLine();
+            Console.Write("Podaj model:");
+            string model = Console.ReadLine();
+            Car newCar = new Car(brand, model);
+            cars.Add(newCar);
+            carDictionary[cars.Count] = newCar;
+            Console.WriteLine($"\nDodano samochód {brand} {model}\n");
+            break;
+          case 2:
+            Console.WriteLine("Lista samochodów:");
+            foreach (Car car in cars)
+            {
+              Console.WriteLine($"{car.Brand} {car.Model}");
+            }
+            break;
+          case 3:
+            Console.Write("Podaj numer samochodu do jazdy:");
+            int carNumber = int.Parse(Console.ReadLine());
+            if (carDictionary.TryGetValue(carNumber,out Car selectedCar))
+            {
+              selectedCar.Drive();
+            }
+            else
+            {
+              Console.WriteLine("\nNieprawidłowy numer samochodu\n");
+            }
+            break;
+        }
+      }
+
+      Console.ReadKey();
+    }
+  }
+}
---
+++ 2c_gr_1/12_2_destruktory/12_2_destruktory_symulator/classes/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_2_destruktory_symulator.classes
+{
+  internal class Car
+  {
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public bool IsDamaged { get; set; }
+
+    public Car(string brand, string model)
+    {
+      Brand = brand;
+      Model = model;
+      IsDamaged = false;
+    }
+
+    public void Drive()
+    {
+      if (IsDamaged)
+      {
+        Console.WriteLine($"Samochód {Brand} {Model} ma awarię!");
+      }
+      else
+      {
+        Console.WriteLine($"Samochód {Brand} {Model} porusza się");
+      }
+    }
+
+    ~Car()
+    {
+      Console.WriteLine($"Samochód {Brand} {Model} jest zezłomowany");
+    }
+
+    public void SimulateRandomDamage()
+    {
+      Random random = new Random();
+
+      int damageType = random.Next( 1, 4 ); //losujemy 1-3
+
+      switch( damageType )
+      {
+        case 1:
+          Console.WriteLine($"Samochód {Brand} {Model} ma przebitą oponę");
+          break;
+        case 2:
+          Console.WriteLine($"Silnik samochodu {Brand} {Model} jest uszkodzony");
+          break;
+        case 3:
+          Console.WriteLine($"Samochód {Brand} {Model} ma uszkodzone światło");
+          break;
+      }
+      IsDamaged = true;
+    }
+  }
+}
